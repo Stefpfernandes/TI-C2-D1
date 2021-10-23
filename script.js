5b1f9004f59df7fe7e654792e98192e81a19cbb4
Stefanie Pereira Fernandes <85763825+Stefpfernandes@users.noreply.github.com>
2021-10-23 20:43:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+
+    const limparFormulario = (endereco) =>{
+        document.querySelector("#rua").value = "";
+        document.querySelector("#bairro").value = "";
+        document.querySelector("#cidade").value = "";
+        document.querySelector("#estado").value = "";
+    }
+
+    const preencherFormulario = (endereco) =>{        
+        document.querySelector("#rua").value = endereco.logradouro;
+        document.querySelector("#bairro").value = endereco.bairro;
+        document.querySelector("#cidade").value = endereco.localidade;
+        document.querySelector("#estado").value = endereco.uf;
+    }
+
+    const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+    const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+    const pesquisarCep = async() => {
+        limparFormulario();
+
+        const cep = document.querySelector("#cep").value;
+        let url = `http://viacep.com.br/ws/${cep}/json/`;
+
+        if(cepValido(cep)){
+            const dados = await fetch(url);
+            const endereco = await dados.json();
+
+            if(endereco.hasOwnProperty('erro')){
+                alert(`O Cep: ${cep} não foi encontrado! 
+                Por favor digite outro.`)
+            } else {
+                preencherFormulario(endereco);
+            }
+        } else {
+            document.querySelector("#rua").value = 'CEP digitado incorretamente!';
+        }
+    }
+
+    document.querySelector("#cep").addEventListener('focusout', pesquisarCep);
+    // const cep = document.querySelector("#cep");
+    // const botao = document.querySelector("#buscar");
+
+    // botao.addEventListener('click', function(){
+        
+    // })
+    // addEventListener('click', pesquisarCep);
+
+    
+}
